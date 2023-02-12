627e40f6438d7017b3db5df4c16f46f9892c83c7
adrianochaita04 <adrian.ochaita@edu.uah.es>
2023-02-12 20:24:42 +0100

    Red colour

--- arcade-demo/main.py
+++ arcade-demo/main.py
@@ -44,7 +44,7 @@ class MenuView(arcade.View):
     def on_draw(self):
         arcade.start_render()
         arcade.draw_text("Menu Screen", WIDTH / 2, HEIGHT / 2,
-                         arcade.color.BLACK, font_size=50, anchor_x="center")
+                         arcade.color.RED, font_size=50, anchor_x="center")
         arcade.draw_text("Click to advance", WIDTH / 2, HEIGHT / 2 - 75,
                          arcade.color.GRAY, font_size=20, anchor_x="center")
 
